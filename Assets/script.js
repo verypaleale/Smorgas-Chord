ca42d209dd7ee1635f60f19d0a9627eeaa33d2b3
Kendra Kwoka <kendrakwoka@gmail.com>
2020-02-04 19:22:00 -0600

    kendra's changes

--- Assets/script.js
+++ Assets/script.js
@@ -9,14 +9,13 @@ var queryLYR;
 var replaceSpace;
 var showLyrics1;
 
-// $(document).on("click", ".listnone", getLyrics);
 $(document).on("click", ".happiButtons", lyricsClick);
+$(document).on("click", ".YEEZY", kanyeTalks);
 
 var retrievedData = localStorage.getItem("songArr");
 if (retrievedData !=null) {
     var songArr2 = JSON.parse(retrievedData);
     if (songArr.length >= 0) {
-        // console.log(songArr2.length)
         for (i = 0; i < songArr2.length; i++) {
             var ul = $("<ul>").attr("class", "listnone");
             var li = $("<li>");
@@ -27,8 +26,9 @@ if (retrievedData !=null) {
     }
 }
 
-$("#searchButton").click(function updateSongList() {
-    $(".showLyricsDiv").text = "";
+// get lyrics and tabs from  search button
+$("#search").click(function updateSongList() {
+    $(".showLyricsDiv").text("");
 
     var currentSong = $("#input").val();
     songArr.push(currentSong);
@@ -39,58 +39,27 @@ $("#searchButton").click(function updateSongList() {
     ul.append(li);
 
     replaceSpace = currentSong.replace(/\s/g, "%20"); 
-
     queryLyrics = "https://api.happi.dev/v1/music?q=" + replaceSpace + "&limit=&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv&type="
 
     $.ajax({
         url: queryLyrics,
         method: "GET"
     }).then(function(response) {
+            $("#songButtons").text("");
             var lyricsHeader = $("<h5>").text("Lyrics Options: ")
-            $("#songList").append(lyricsHeader);
+            $("#songButtons").append(lyricsHeader);
 
             for (i = 0; i < response.length; i++) {
-                buttonDIV = $("<button>").attr("class", "happiButtons");
+                buttonDIV = $("<div>").attr("class", "happiButtons");
                 titleDiv = $("<div>").text("Title: " + response.result[i].track);
                 artistDiv = $("<div>").text("Artist: " + response.result[i].artist);
                 lyrics = response.result[i].api_lyrics + "?&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv";
                 buttonDIV.val(lyrics);
                 $(buttonDIV).append(titleDiv);
                 $(buttonDIV).append(artistDiv);
-                $("#songList").append(buttonDIV);
-                // queryLyricsARR.push(lyrics);
-                // console.log("array: " + queryLyricsARR);
-
-                queryLYR = lyrics;
-
-                // $.ajax({
-                //     url: queryLYR,
-                //     method: "GET"
-                // }).then(function(response) {
-                //     // if (response.success === true) {
-                //     // $(".showLyricsDiv").text("");
-                //     // $(lyricsHeader).text("");
-                //     // $(".happiButtons").text("");
-
-                //     // $(buttonDIV).append(titleDiv);
-                //     // $(buttonDIV).append(artistDiv);
-                //     // $("#songList").append(buttonDIV);
-        
-                //     // showLyrics1 = response.result.lyrics;
-                //     // $(".showLyricsDiv").text(showLyrics1);
-                //     // }
-                //     if (response.success === true) {
-                //         // $(".showLyricsDiv").val() = "";
-                //         $(".showLyricsDiv").text = "";
-            
-                //         var showLyrics = response.result.lyrics;
-                        
-                //         $(".showLyricsDiv").append(showLyrics);
-                //     } else if (response.success === false) {
-                //         alert("Sorry, there are no lyrics on file for that option.")
-                //     }
-                // })
-                
+                $("#songButtons").append(buttonDIV);
+          
+                queryLYR = lyrics;                
             }
     })
 
@@ -100,126 +69,133 @@ $("#searchButton").click(function updateSongList() {
             url: queryTabs,
             method: "GET"
             }).then(function(response) {
-            var tabsInfoHeader = $("<h5>").text("Tab Options: ")
-            $(".showTabsDiv").append(tabsInfoHeader);
-            $(".showTabsDiv").text("");
+                $("#tabList").text("");
+                var tabsInfoHeader = $("<h5>").text("Tab Options: ")
+                $("#tabList").append(tabsInfoHeader);
 
-            
                 for (i = 0; i < response.length; i++) {
                     var tabButtons = $("<div>").attr("class", "songsterButtons");
                     var titleDiv = $("<li>").text("Title: " + response[i].title);
                     var artistDiv = $("<li>").text("Artist: " + response[i].artist.name);
                     var queryURL = "https://www.songsterr.com/a/wa/song?id=" + response[i].id;
-                    var str = "Click me for tab info" + i;
+                    var str = "Click me for tab info!";
                     var result = str.link(queryURL); 
 
                     $(tabButtons).append(titleDiv);
                     $(tabButtons).append(artistDiv);
                     $(tabButtons).append(result);
-                    $(".showTabsDiv").append(tabButtons);
+                    $("#tabList").append(tabButtons);
                 }
             })
-
-
-    $("#songList").prepend(ul);
+    $("#songButtons").prepend(ul);
     localStorage.getItem("songArr", songArr);
 })
 
 function lyricsClick() {
     queryLYR = $(this).val();
-    console.log(queryLYR)
 
     $.ajax({
         url: queryLYR,
         method: "GET"
     }).then(function(response) {
         if (response.success === true) {
-            // $(".showLyricsDiv").val() = "";
             $(".showLyricsDiv").text("");
             var showLyrics = response.result.lyrics;
             $(".showLyricsDiv").append(showLyrics);
-        } else if (response.success === false) {
+        } else {
+            var btn = document.getElementById("myBtn");
+            var span = document.getElementsByClassName("close")[0];
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
             alert("Sorry, there are no lyrics on file for that option.")
         }
-    }).catch(function() {
-        alert("This particular song doesn't have lyrics on file.");
-    });
+    })
 }
 
-// ----------------------------------------------------------------------------------
 
-// function getLyrics() {
-//     songName = $(this).text();
-//     replaceSpace = songName.replace(/\s/g, "%20"); 
+// get lyrics and tabs from list history
+$(".listnone").click(function updateSongList() {
+    $(".showLyricsDiv").text("");
 
-//     queryLyrics = "https://api.happi.dev/v1/music?q=" + replaceSpace + "&limit=&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv&type="
+    var currentSong = $(this).text();
+    songArr.push(currentSong);
+    localStorage.setItem("songArr", JSON.stringify(songArr));
+    var ul = $("<ul>").attr("class", "listnone");
+    var li = $("<li>");
+    li.append(currentSong);
 
-//     $.ajax({
-//         url: queryLyrics,
-//         method: "GET"
-//     }).then(function(response) {
-//             var lyricsHeader = $("<h5>").text("Lyrics Options: ")
-//             $(".musicInfoDiv").append(lyricsHeader);
+    replaceSpace = currentSong.replace(/\s/g, "%20"); 
+    queryLyrics = "https://api.happi.dev/v1/music?q=" + replaceSpace + "&limit=&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv&type="
 
-//             for (i = 0; i < response.length; i++) {
-//                 buttonDIV = $("<button>").attr("class", "happiButtons");
-//                 titleDiv = $("<div>").text("Title: " + response.result[i].track);
-//                 artistDiv = $("<div>").text("Artist: " + response.result[i].artist);
-//                 lyrics = response.result[i].api_lyrics + "?&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv";
-//                 buttonDIV.val(lyrics);
-//                 queryLyricsARR.push(lyrics);
-//                 console.log("array: " + queryLyricsARR)
+    $.ajax({
+        url: queryLyrics,
+        method: "GET"
+    }).then(function(response) {
+        $("#songButtons").text("");
+            var lyricsHeader = $("<h5>").text("Lyrics Options: ")
+            $("#songButtons").append(lyricsHeader);
 
-//                 $(buttonDIV).append(titleDiv);
-//                 $(buttonDIV).append(artistDiv);
-//                 $(".musicInfoDiv").append(buttonDIV);
-//             }
-//     });
+            for (i = 0; i < response.length; i++) {
+                buttonDIV = $("<div>").attr("class", "happiButtons");
+                titleDiv = $("<div>").text("Title: " + response.result[i].track);
+                artistDiv = $("<div>").text("Artist: " + response.result[i].artist);
+                lyrics = response.result[i].api_lyrics + "?&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv";
+                buttonDIV.val(lyrics);
+                $(buttonDIV).append(titleDiv);
+                $(buttonDIV).append(artistDiv);
+                $("#songButtons").append(buttonDIV);
 
-//     var queryTabs = "https://www.songsterr.com/a/ra/songs.json?pattern=" + replaceSpace;
+                queryLYR = lyrics;  
+            }
+    })
 
-//     $.ajax({
-//         url: queryTabs,
-//         method: "GET"
-//         }).then(function(response) {
-//         var tabsInfoHeader = $("<h5>").text("Tab Options: ")
-//         $(".tabsInfoDIv").append(tabsInfoHeader);
-        
-//             for (i = 0; i < response.length; i++) {
-//                 var tabButtons = $("<div>").attr("class", "songsterButtons");
-//                 var titleDiv = $("<li>").text("Title: " + response[i].title);
-//                 var artistDiv = $("<li>").text("Artist: " + response[i].artist.name);
-//                 var queryURL = "https://www.songsterr.com/a/wa/song?id=" + response[i].id;
-//                 var str = "Click me for tab info" + i;
-//                 var result = str.link(queryURL); 
+        var queryTabs = "https://www.songsterr.com/a/ra/songs.json?pattern=" + replaceSpace;
 
-//                 $(tabButtons).append(titleDiv);
-//                 $(tabButtons).append(artistDiv);
-//                 $(tabButtons).append(result);  
-//                 $(".tabsInfoDIv").append(tabButtons)
-//             }
-//         })
-// }
+        $.ajax({
+            url: queryTabs,
+            method: "GET"
+            }).then(function(response) {
+                $("#tabList").text("");
+                var tabsInfoHeader = $("<h5>").text("Tab Options: ")
+                $("#tabList").append(tabsInfoHeader);
+
+                for (i = 0; i < response.length; i++) {
+                    var tabButtons = $("<div>").attr("class", "songsterButtons");
+                    var titleDiv = $("<li>").text("Title: " + response[i].title);
+                    var artistDiv = $("<li>").text("Artist: " + response[i].artist.name);
+                    var queryURL = "https://www.songsterr.com/a/wa/song?id=" + response[i].id;
+                    var str = "Click me for tab info!";
+                    var result = str.link(queryURL); 
 
 
+                    $(tabButtons).append(titleDiv);
+                    $(tabButtons).append(artistDiv);
+                    $(tabButtons).append(result);
+                    $("#tabList").append(tabButtons);
+                }
+            })
 
-// function lyricsClick() {
-//     var queryLYR = $(this).val();
+    $("#songButtons").prepend(ul);
+    localStorage.getItem("songArr", songArr);
+})
 
-//     $.ajax({
-//         url: queryLYR,
-//         method: "GET"
-//     }).then(function(response) {
-//         if (response.success === true) {
-//             // $(".showLyricsDiv").val() = "";
-//             $(".showLyricsDiv").text = "";
-
-//             var showLyrics = response.result.lyrics;
-//             $(".showLyricsDiv").append(showLyrics);
-//         } else if (response.success === false) {
-//             alert("Sorry, there are no lyrics on file for that option.")
-//         }
-//     }).catch(function() {
-//         alert("This particular song doesn't have lyrics on file.");
-//     });
-// }
+// Kanye API Button
+function kanyeTalks() {
+    var kanyeURL = "https://api.kanye.rest/?format=text"
+    $.ajax({
+        url: kanyeURL,
+        method: "GET"
+    }).then(function(kanyeResponse) {
+        console.log(kanyeResponse);
+        $("yeezy-quote").text(kanyeResponse);
+    })
+}

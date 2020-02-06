9389126a52d2a67eec1470ab53057180d56a377c
Kendra Kwoka <kendrakwoka@gmail.com>
2020-02-06 17:04:13 -0600

    kk removed comments on js file

--- Assets/script.js
+++ Assets/script.js
@@ -14,8 +14,6 @@ $(document).on("click", "#YEEZY", kanyeTalks);
 $(document).on("click", "#search", currentSongSearch);
 $(document).on("click", ".listnone", currentSongListnone);
 
-
-
 var retrievedData = localStorage.getItem("songArr");
 if (retrievedData !=null) {
     var songArr2 = JSON.parse(retrievedData);
@@ -48,19 +46,9 @@ function currentSongListnone() {
 }
 
 // get lyrics and tabs from  search button
-// $("#search").click(
 function updateSongList() {
     $(".showLyricsDiv").text("");
 
-    // var currentSong = $("#input").val();
-    // songArr.push(currentSong);
-    // localStorage.setItem("songArr", JSON.stringify(songArr));
-    // var ul = $("<ul>").attr("class", "listnone");
-    // var li = $("<li>");
-    // li.append(currentSong);
-    // ul.append(li);
-    // $("#searchedList").append(ul);
-    console.log(currentSong);
     replaceSpace = currentSong.replace(/\s/g, "%20"); 
     console.log("replace: "+ replaceSpace)
     queryLyrics = "https://api.happi.dev/v1/music?q=" + replaceSpace + "&limit=&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv&type="
@@ -118,8 +106,6 @@ function updateSongList() {
                     $("#tabList").append(tabButtons);
                 }
             })
-    // $("#songButtons").prepend(ul);
-    // localStorage.getItem("songArr", songArr);
 }
 
 function lyricsClick() {
@@ -137,76 +123,6 @@ function lyricsClick() {
         } 
     })
 }
-
-// get lyrics and tabs from list history
-// $(".listnone").click(function updateSongList() {
-//     $(".showLyricsDiv").text("");
-
-//     var currentSong = $(this).text();
-//     songArr.push(currentSong);
-//     localStorage.setItem("songArr", JSON.stringify(songArr));
-//     var ul = $("<ul>").attr("class", "listnone");
-//     var li = $("<li>");
-//     li.append(currentSong);
-
-//     replaceSpace = currentSong.replace(/\s/g, "%20"); 
-//     queryLyrics = "https://api.happi.dev/v1/music?q=" + replaceSpace + "&limit=&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv&type="
-
-//     $.ajax({
-//         url: queryLyrics,
-//         method: "GET"
-//     }).then(function(response) {
-//         $("#songButtons").text("");
-//             var lyricsHeader = $("<h5>").text("Lyrics Options: ")
-//             $("#songButtons").append(lyricsHeader);
-
-//             for (i = 0; i < response.length; i++) {
-//                 buttonDIV = $("<div>").attr("class", "happiButtons");
-//                 buttonDIV.attr("id", `test${i}`);
-//                 titleDiv = $("<div>").text("Title: " + response.result[i].track);
-//                 artistDiv = $("<div>").text("Artist: " + response.result[i].artist);
-//                 lyrics = response.result[i].api_lyrics + "?&apikey=05580c9wJXOa2YrFZUJlxtMDKREEexMldmTAHlmwb7Uk62acRmtbkJIv";
-//                 buttonDIV.val(lyrics);
-//                 $(buttonDIV).append(titleDiv);
-//                 $(buttonDIV).append(artistDiv);
-//                 $("#songButtons").append(buttonDIV);
-
-//                 queryLYR = lyrics;  
-//             }
-//             for (i = 0; i < response.length; i++) {
-//                 tippy(`#test${i}`, {
-//                     content: "Click me to see if I have lyrics!"
-//                 });
-//             }
-//     })
-
-//         var queryTabs = "https://www.songsterr.com/a/ra/songs.json?pattern=" + replaceSpace;
-
-//         $.ajax({
-//             url: queryTabs,
-//             method: "GET"
-//             }).then(function(response) {
-//                 $("#tabList").text("");
-//                 var tabsInfoHeader = $("<h5>").text("Tab Options: ")
-//                 $("#tabList").append(tabsInfoHeader);
-
-//                 for (i = 0; i < response.length; i++) {
-//                     var tabButtons = $("<div>").attr("class", "songsterButtons");
-//                     var titleDiv = $("<li>").text("Title: " + response[i].title);
-//                     var artistDiv = $("<li>").text("Artist: " + response[i].artist.name);
-//                     var queryURL = "https://www.songsterr.com/a/wa/song?id=" + response[i].id;
-//                     var str = "Click me for tab info!";
-//                     var result = str.link(queryURL); 
-
-//                     $(tabButtons).append(titleDiv);
-//                     $(tabButtons).append(artistDiv);
-//                     $(tabButtons).append(result);
-//                     $("#tabList").append(tabButtons);
-//                 }
-//             })
-//     $("#songButtons").prepend(ul);
-//     localStorage.getItem("songArr", songArr);
-// })
 
 // Kanye API Button
 function kanyeTalks() {

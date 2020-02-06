e77237b8e4d06429309f7572fa6ce0b2a9dd03d4
roldie9 <aroldoreyes9@gmail.com>
2020-02-05 20:31:28 -0600

    final touches

--- Assets/script.js
+++ Assets/script.js
@@ -10,7 +10,7 @@ var replaceSpace;
 var showLyrics1;
 
 $(document).on("click", ".happiButtons", lyricsClick);
-$(document).on("click", "#YEEZY", kanyeTalks);
+$(document).on("click", "#yeezy", kanyeTalks);
 
 var retrievedData = localStorage.getItem("songArr");
 if (retrievedData !=null) {
